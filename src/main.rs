7f94fd7c3197194c10e4eb8b32e4238d6099c13b
Xorycode <HelloWorld791@protonmail.com>
2023-12-23 21:26:56 +0200

    init: add code

---
+++ src/main.rs
@@ -0,0 +1,97 @@
+use std::path::PathBuf;
+use std::fs::{read_to_string, File, OpenOptions};
+use std::env::var_os;
+use std::io;
+use std::io::{Write, BufWriter};
+
+fn add_item(todo_file_path: PathBuf) {
+    // Get line to add
+    print!("Enter item to add: ");
+    io::stdout().flush().expect("Failed to flush STDOUT. May God have mercy on your soul, because this spells disaster.");
+    let mut to_add = String::new();
+    io::stdin().read_line(&mut to_add).expect("Failed to read STDIN. May God have mercy on your soul, because this spells disaster.");
+
+    // Slight modifications
+    to_add.insert_str(0, "- ");
+    let to_add = to_add.trim();
+
+    let mut file = OpenOptions::new()
+        .write(true)
+        .append(true)
+        .open(todo_file_path)
+        .expect("Could not open todo.txt");
+
+    writeln!(file, "{}", to_add).expect("Failed to write to todo.txt");
+    println!("");
+    println!("");
+    println!("{}", to_add);
+    
+}
+
+fn remove_item(todo_file_path: PathBuf) {
+    let mut lines: Vec<String> = read_to_string(&todo_file_path)
+        .expect("Could not read todo.txt")
+        .lines()
+        .map(String::from)
+        .collect();
+    
+    println!("\nWhich item would you like to remove?");
+
+    let mut counter: u32 = 1;
+    for line in &lines {
+        println!("{}: {}", counter, line);
+        counter += 1;
+    }
+        
+    let mut option = String::new();
+
+    print!("\n> ");
+    io::stdout().flush().expect("Failed to flush STDOUT. May God have mercy on your soul, because this spells disaster.");
+    io::stdin().read_line(&mut option).expect("Failed to read STDIN. May God have mercy on your soul, because this spells disaster.");
+
+    let option: u32 = option
+        .trim()
+        .parse::<u32>()
+        .expect("Couldn't parse user's choice.") 
+        - 1;
+
+    lines.remove(option.try_into().expect("Could not convert u32 to usize. ???"));
+
+    let file = File::create(todo_file_path).expect("Could not open todo.txt");
+    let mut file_writer = BufWriter::new(file);
+    
+    for line in lines {
+        write!(file_writer, "{}\n", line).expect("Could not write to todo.txt.");
+        file_writer.flush().expect("Failed to flush changes to todo.txt");
+    }
+}
+
+
+fn main() -> std::io::Result<()> {
+    loop {
+        let user_home = var_os("HOME").expect("Could not get $HOME");
+        let mut todo_file_path = PathBuf::from(user_home);
+        todo_file_path.push("todo");
+        todo_file_path.set_extension("txt");
+        if todo_file_path.exists() {
+            let todo_contents = read_to_string(&todo_file_path).expect("Todo file found but cannot be opened.");
+            println!("Current todo list is:");
+            println!("{}", todo_contents);
+        } else {
+            File::create(&todo_file_path).expect("Creation failed.");
+        }
+
+        println!("
+What would you like to do?
+1) Add item to the todo list
+2) Remove item from the todo list
+3) Exit");
+
+
+        let mut option = String::new();
+        io::stdin().read_line(&mut option)?;
+        
+    }
+    
+    Ok(())
+}
